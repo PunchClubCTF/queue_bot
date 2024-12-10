68cef475824f88381272019e9679fe72c2314886
MixInSpace <mi.maruas@yandex.ru>
2024-12-11 01:51:32 +0300

    commit

--- bot/app/routes.py
+++ bot/app/routes.py
@@ -10,36 +10,36 @@ def index():
 
     return render_template('index.html')
 
-@main.route('/profile')
-def profile():
-    return render_template('index.html')
-
-@main.route('/get_queue')
-def profile():
-    return json.load()
-
-@main.route('/add_user_to_queue')
-def profile():
-    return render_template('index.html')
-
-@main.route('/delete_user_from_queue')
-def profile():
-    return render_template('index.html')
-
-
-
-admin = Blueprint('admin', __name__)
-
-@admin.route('/')
-def profile():
-    return render_template('index.html')
-
-@admin.route('/add_lab')
-def profile():
-    return render_template('index.html')
-
-@admin.route('/delete_lab')
-def profile():
-    return render_template('index.html')
-
+# @main.route('/profile')
+# def profile():
+#     return render_template('index.html')
+#
+# @main.route('/get_queue')
+# def profile():
+#     return json.load()
+#
+# @main.route('/add_user_to_queue')
+# def profile():
+#     return render_template('index.html')
+#
+# @main.route('/delete_user_from_queue')
+# def profile():
+#     return render_template('index.html')
+#
+#
+#
+# admin = Blueprint('admin', __name__)
+#
+# @admin.route('/')
+# def profile():
+#     return render_template('index.html')
+#
+# @admin.route('/add_lab')
+# def profile():
+#     return render_template('index.html')
+#
+# @admin.route('/delete_lab')
+# def profile():
+#     return render_template('index.html')
+#
 
