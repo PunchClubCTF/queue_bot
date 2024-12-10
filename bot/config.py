ba24c55591fea9826a4d74d40634ac99f6a4ea44
MixInSpace <mi.maruas@yandex.ru>
2024-12-11 01:47:30 +0300

    commit

--- bot/config.py
+++ bot/config.py
@@ -2,5 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://myuser:mypassword@localhost:5432/mydb'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        f"postgresql://{os.environ.get('DB_USER', 'myuser')}:" \
+        f"{os.environ.get('DB_PASSWORD', 'mypassword')}@" \
+        f"{os.environ.get('DB_HOST', 'localhost')}:" \
+        f"{os.environ.get('DB_PORT', '5432')}/" \
+        f"{os.environ.get('DB_NAME', 'mydb')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
