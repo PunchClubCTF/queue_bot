50a10ec3d22f578ac359bb8b117c14785cafb7e0
MixInSpace <mi.maruas@yandex.ru>
2024-12-11 01:58:32 +0300

    commit

--- bot/app/routes.py
+++ bot/app/routes.py
@@ -5,10 +5,10 @@ from . import db
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def index():
-
-    return "main"
+    return render_template('index.html')
 
 # @main.route('/profile')
 # def profile():
