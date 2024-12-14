3205e3b93c1beed63a5a6948f8268fc252ff3440
MixInSpace <mi.maruas@yandex.ru>
2024-12-15 02:07:47 +0300

    commit

--- bot/run.py
+++ bot/run.py
@@ -8,7 +8,7 @@ import threading
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
-
     bot_thread = threading.Thread(target=run_bot)
     bot_thread.start()
+
+    app.run(host='0.0.0.0', debug=True)
